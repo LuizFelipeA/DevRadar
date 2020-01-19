48f517358b8dca6530b31e6d84de42c97007ab34
luiz.felipe.da.silva@avanade.com <luiz.felipe.da.silva@avanade.com>
2020-01-19 19:55:29 -0300

    Back-End application

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,8 @@ require('dotenv/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
 const routes = require('./routes');
 
 const app = express();
@@ -12,6 +14,7 @@ mongoose.connect(process.env.MONGOO_CONNECT_URL,{
     useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
